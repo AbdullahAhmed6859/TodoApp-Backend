8b876087aa48d483a03e35b278990810473b38f8
Abdullah Ahmed <abdullahahmed2004@gmail.com>
2025-05-17 01:10:08 +0500

    Cors added

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,28 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 const ENV = process.env.ENV || "DEV";
 
 const app = express();
+const whitelist = ["http://localhost:5173"];
+
+const corsOptions = {
+  // @ts-ignore
+  origin: function (origin, callback) {
+    if (!origin || whitelist.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.status(200).json({
