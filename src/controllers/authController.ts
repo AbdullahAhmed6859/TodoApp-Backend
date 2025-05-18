b7c33ac0bd8d3e10773d0569c9d5906fddd15fb8
Abdullah Ahmed <abdullahahmed2004@gmail.com>
2025-05-18 14:15:24 +0500

    MVC - updated bcrypt hashing

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -2,7 +2,7 @@ import { loginSchema, signupSchema } from "../zodSchemas/authSchemas";
 import { createUser, findUserByEmail } from "../models/userModel";
 import { generateToken } from "../utils/jwt";
 import { Request, Response } from "express";
-import bcrypt from "bcrypt";
+import { compare } from "bcrypt";
 
 export async function signUp(req: Request, res: Response) {
   const result = signupSchema.safeParse(req.body);
@@ -21,14 +21,7 @@ export async function signUp(req: Request, res: Response) {
       return;
     }
 
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    const newUser = await createUser(
-      firstName,
-      lastName,
-      email,
-      hashedPassword
-    );
+    const newUser = await createUser(firstName, lastName, email, password);
 
     const token = generateToken(newUser.id);
 
@@ -60,7 +53,7 @@ export async function logIn(req: Request, res: Response) {
       return;
     }
 
-    const valid = await bcrypt.compare(password, user.password);
+    const valid = await compare(password, user.password);
     if (!valid) {
       res.status(401).json({ error: "Invalid email or password" });
       return;
--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -1,11 +1,14 @@
 import { pool } from "../db/pool";
+import { hash } from "bcrypt";
 
 export async function createUser(
   firstName: string,
   lastName: string,
   email: string,
-  hashedPassword: string
+  password: string
 ) {
+  const hashedPassword = await hash(password, 10);
+
   const result = await pool.query(
     `INSERT INTO users (first_name, last_name, email, password)
     VALUES ($1, $2, $3, $4)
