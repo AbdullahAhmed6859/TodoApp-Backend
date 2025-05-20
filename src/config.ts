50871a9a45ac2ef300b837aae9df8db4d53f5473
Abdullah Ahmed <abdullah.a@systemsltd.com>
2025-05-20 13:28:34 +0500

    env variables updated

--- src/config.ts
+++ src/config.ts
@@ -3,5 +3,6 @@ dotenv.config();
 
 export const PORT = process.env.PORT || 3000;
 export const ENV = process.env.ENV || "DEV";
-export const JWT_SECRET = process.env.JWT_SECRET || "your_default_secret";
-export const DATABASE_URL = process.env.DATABASE_URL || "NO_URL_PROVIDED";
+export const JWT_SECRET = process.env.JWT_SECRET || "123123";
+export const DATABASE_URL = process.env.DATABASE_URL || "";
+if (!DATABASE_URL) console.error("NO DATABASE URL Provided");
